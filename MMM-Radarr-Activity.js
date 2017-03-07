e7b0faf56481fa029cbd26bbd9e4ba2838e9b59c
s-cotton <cot.s.ton@gmail.com>
2017-03-07 15:21:20 -0500

    Added missing moment library in getScripts

--- MMM-Radarr-Activity.js
+++ MMM-Radarr-Activity.js
@@ -169,6 +169,7 @@ Module.register("MMM-Radarr-Activity", {
 
     getScripts: function() {
         return [
+            'moment.js',
             'https://code.jquery.com/jquery-2.2.3.min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js',
